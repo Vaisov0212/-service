b4c0a745707411154aee672a74ddcb8f361dd54c
Vaisov0212 <karval117@mail.ru>
2021-05-10 21:27:00 +0500

    create layouts

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,115 @@
+
+	<!-- Footer section -->
+	<footer class="footer-section spad">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-6">
+					<div class="footer-widget about-widget">
+						<img src="img/logo-light.png" alt="">
+						<p>Lorem ipsum dolor sit amet, consec-tetur adipiscing elit. Quisque orci purus, sodales in est quis, blandit sollicitudin est. Nam ornare ipsum ac accumsan auctor. Donec consequat arcu et commodo interdum. </p>
+						<div class="footer-social">
+							<a href=""><i class="fa fa-facebook"></i></a>
+							<a href=""><i class="fa fa-twitter"></i></a>
+							<a href=""><i class="fa fa-dribbble"></i></a>
+							<a href=""><i class="fa fa-behance"></i></a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<div class="footer-widget">
+						<h2 class="fw-title">Useful Resources</h2>
+						<ul>
+							<li><a href="">Jobs Vacancies</a></li>
+							<li><a href="">Client Testimonials</a></li>
+							<li><a href="">Green  Energy</a></li>
+							<li><a href="">Chemical Research</a></li>
+							<li><a href="">Oil Extractions</a></li>
+							<li><a href="">About our Work</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<div class="footer-widget">
+						<h2 class="fw-title">Our Solutions</h2>
+						<ul>
+							<li><a href="">Metal Industry</a></li>
+							<li><a href="">Agricultural Engineering</a></li>
+							<li><a href="">Green  Energy</a></li>
+							<li><a href="">Chemical Research</a></li>
+							<li><a href="">Oil Extractions</a></li>
+							<li><a href="">Manufactoring</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-7">
+					<div class="footer-widget">
+						<h2 class="fw-title">Contact Us</h2>
+						<div class="footer-info-box">
+							<div class="fib-icon">
+								<img src="img/icons/map-marker.png" alt="" class="">
+							</div>
+							<div class="fib-text">
+								<p>Main Str, no 23 NY,<br>New York PK 23589</p>
+							</div>
+						</div>
+						<div class="footer-info-box">
+							<div class="fib-icon">
+								<img src="img/icons/phone.png" alt="" class="">
+							</div>
+							<div class="fib-text">
+								<p>+546 990221 123<br>contact@industryalinc.com</p>
+							</div>
+						</div>
+						<form class="footer-search">
+							<input type="text" placeholder="Search">
+							<button><i class="fa fa-search"></i></button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="footer-buttom">
+			<div class="container">
+			<div class="row">
+				<div class="col-lg-4 order-2 order-lg-1 p-0">
+					<div class="copyright"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></div>
+				</div>
+				<div class="col-lg-7 order-1 order-lg-2 p-0">
+					<ul class="footer-menu">
+						<li class="active"><a href="index.html">Home</a></li>
+						<li><a href="about.html">About us</a></li>
+						<li><a href="solutions.html">Solutions</a></li>
+						<li><a href="blog.html">Blog</a></li>
+						<li><a href="contact.html">Contact</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		</div>
+	</footer>
+	<!-- Footer section end -->
+
+	<!-- Search model -->
+	<div class="search-model">
+		<div class="h-100 d-flex align-items-center justify-content-center">
+			<div class="search-close-switch">+</div>
+			<form class="search-model-form">
+				<input type="text" id="search-input" placeholder="Search here.....">
+			</form>
+		</div>
+	</div>
+	<!-- Search model end -->
+
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.slicknav.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/circle-progress.min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="uz">
+<head>
+	<title>Industry.INC | HTML Template</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="Industry.INC HTML Template">
+	<meta name="keywords" content="industry, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- Favicon -->
+	<link href="img/favicon.ico" rel="shortcut icon"/>
+
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i&display=swap" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/magnific-popup.css"/>
+	<link rel="stylesheet" href="css/slicknav.min.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.min.css"/>
+
+	<!-- Main Stylesheets -->
+	<link rel="stylesheet" href="css/style.css"/>
+
+
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	<!-- Page Preloder -->
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+
+	<!-- Header section  -->
+	<header class="header-section clearfix">
+		<div class="header-top">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-6">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					</div>
+					<div class="col-md-6 text-md-right">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="site-navbar">
+			<!-- Logo -->
+			<a href="index.html" class="site-logo">
+				<img src="img/logo.png" alt="">
+			</a>
+			<div class="header-right">
+				<div class="header-info-box">
+					<div class="hib-icon">
+						<img src="img/icons/phone.png" alt="" class="">
+					</div>
+					<div class="hib-text">
+						<h6>+546 990221 123</h6>
+						<p>contact@industryalinc.com</p>
+					</div>
+				</div>
+				<div class="header-info-box">
+					<div class="hib-icon">
+						<img src="img/icons/map-marker.png" alt="" class="">
+					</div>
+					<div class="hib-text">
+						<h6>Main Str, no 23</h6>
+						<p>NY, New York PK 23589</p>
+					</div>
+				</div>
+				<button class="search-switch"><i class="fa fa-search"></i></button>
+			</div>
+			<!-- Menu -->
+			<nav class="site-nav-menu">
+				<ul>
+					<li class="active"><a href="index.html">Home</a></li>
+					<li><a href="about.html">About us</a></li>
+					<li><a href="solutions.html">Solutions</a>
+						<ul class="sub-menu">
+							<li><a href="elements.html">Elements</a></li>
+						</ul>
+					</li>
+					<li><a href="blog.html">Blog</a></li>
+					<li><a href="contact.html">Contact</a></li>
+				</ul>
+			</nav>
+
+		</div>
+	</header>
+	<!-- Header section end  -->
+
